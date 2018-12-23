67b32667b3d81d20ecb76aa8efca9bc6e51a572d
nldaseul <nldaseul@gmail.com>
2018-12-23 14:27:08 +0900

    Add signup/signin feature

--- partner/urls.py
+++ partner/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
-from .views import signup
+from .views import index, signup, login
 
 urlpatterns = [
-    url(r'^$', signup, name="signup"),
+    url(r'^$', index, name="index"),
+    url(r'^signup/$', signup, name="signup"),
+    url(r'^login/$', login, name="login"),
 ]
--- partner/views.py
+++ partner/views.py
@@ -1,7 +1,38 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def index(request):
+    ctx = {}
+    return render(request, "index.html", ctx)
+
+def login(request):
+    if request.method == "GET":
+        pass
+
+    elif request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect("/partner/")
+        else:
+            pass
+
+    ctx = {}
+    return render(request, "login.html", ctx)
 
 def signup(request):
+    if request.method == "GET":
+        pass
+
+    elif request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        user = User.objects.create_user(username, email, password)
+
     ctx = {}
     return render(request, "signup.html", ctx)
